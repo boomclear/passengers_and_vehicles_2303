adf4185b187d188ac754b913ccb0a451a484df6a
Austin Woods <austinwoods99@gmail.com>
2023-04-05 11:23:29 -0400

    Add speeding and passenger methods to Vehicle

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe Vehicle do
     expect(vehicle.make).to eq "Honda"
     expect(vehicle.model).to eq "Civic"
   end
+
+  it "can speed" do 
+    vehicle = Vehicle.new("2001", "Honda", "Civic") 
+    
+    expect(vehicle.speeding?).to be false
+    
+    vehicle.speed
+    
+    expect(vehicle.speeding?).to be true
+  end
+
+  it "can have passengers and list them" do
+    vehicle = Vehicle.new("2001", "Honda", "Civic") 
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+    jude = Passenger.new({"name" => "Jude", "age" => 20})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+    expect(vehicle.passengers).to eq([])
+    
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor) 
+
+    expect(vehicle.passengers).to eq([charlie, jude, taylor])
+  end
+
 end
